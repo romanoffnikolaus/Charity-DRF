7d5390ff2375081d97714eb98e8ab87257822a02
Александр Маурус <alexmaurus@MacBook-Pro-MSP.local>
2023-03-26 19:21:04 +0600

    all dontaions view, images to charity programs

--- payment/urls.py
+++ payment/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('payment_process/', views.payment_process, name='payment_process'),
     path('payment_done/', views.payment_done, name='payment_done'),
     path('payment_cancelled/', views.payment_cancelled, name='payment_cancelled'),
+    path('all_donations/', views.AllDonationListView.as_view(), name='all_dontaions')
 ]
 
